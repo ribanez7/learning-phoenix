59f948181ea235d6e42e58867c8870b6e7c4f496
Rubén Ibáñez Carmona <ruben.ibc@gmail.com>
2018-08-10 13:55:31 +0200

    Sales context tested and minimal implemented

---
+++ lib/storex/sales/cart.ex
@@ -0,0 +1,17 @@
+defmodule Storex.Sales.Cart do
+  use Ecto.Schema
+  import Ecto.Changeset
+  alias Storex.Sales.Cart
+
+  schema "sales_carts" do
+    has_many :line_items, Storex.Sales.LineItem
+    timestamps()
+  end
+
+  @doc false
+  def changeset(cart, attrs) do
+    cart
+    |> cast(attrs, [])
+    |> validate_required([])
+  end
+end
---
+++ lib/storex/sales/line_item.ex
@@ -0,0 +1,19 @@
+defmodule Storex.Sales.LineItem do
+  use Ecto.Schema
+  import Ecto.Changeset
+  alias Storex.Sales.LineItem
+
+  schema "sales_line_items" do
+    belongs_to :cart, Storex.Sales.Cart
+    belongs_to :book, Storex.Store.Book
+    field :quantity, :integer
+    timestamps()
+  end
+
+  @doc false
+  def changeset(line_item, attrs) do
+    line_item
+    |> cast(attrs, [:quantity])
+    |> validate_required([:quantity])
+  end
+end
